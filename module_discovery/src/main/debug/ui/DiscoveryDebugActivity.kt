8eff0ab82debfa7386f59aa48caeafe363b37e2c
Piskr0613 <2804239455@qq.com>
2023-07-16 15:39:29 +0800

    :bug: 完成了模块的搭建，并可以进行单模块调试

---
+++ module_discovery/src/main/debug/ui/DiscoveryDebugActivity.kt
@@ -0,0 +1,12 @@
+package ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.module.discovery.R
+
+class DiscoveryDebugActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discovery_debug)
+    }
+}
---
+++ module_square/src/main/java/com/test/module/square/SquareMainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.module.square
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SquareMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_square_main)
+    }
+}
