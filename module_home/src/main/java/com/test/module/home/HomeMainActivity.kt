e9bf5d876ab39882aa5201bd7ec0e0f91125f606
Piskr0613 <2804239455@qq.com>
2023-07-17 19:47:24 +0800

    :bug: 不完全修复了页面跳转闪退的bug

--- module_home/src/main/java/com/test/module/home/HomeMainActivity.kt
+++ module_home/src/main/java/com/test/module/home/HomeMainActivity.kt
@@ -8,25 +8,9 @@ import androidx.fragment.app.FragmentContainerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeMainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var fragmentContainerView: FragmentContainerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_main)
-        bottomNavigationView=findViewById(R.id.bottom_navigation_view)
-        fragmentContainerView=findViewById(R.id.container_view)
-
-        val fragmentMap:MutableMap<Int,Fragment> =HashMap()
-        val homeFragment= HomeFragment()
-        fragmentMap[R.id.home]=homeFragment
-
-        bottomNavigationView.setOnItemSelectedListener{item->
-            when(item.itemId){
-                R.id.home->supportFragmentManager.beginTransaction().replace(R.id.container_view,fragmentMap[R.id.home]!!).commit()
-                R.id.square->supportFragmentManager.beginTransaction().replace(R.id.container_view,fragmentMap[R.id.square]!!).commit()
-            }
-            true
-        }
     }
 }
