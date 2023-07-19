c1b9a5eb6d094edaaaaecdfe858901883cd4aaf6
Piskr0613 <2804239455@qq.com>
2023-07-19 11:54:31 +0800

    :bug: test test

--- app/src/main/java/com/example/openeyessss/MainActivity.kt
+++ app/src/main/java/com/example/openeyessss/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.test.module.discovery.fragment.DiscoveryFragment
 import com.test.module.home.HomeFragment
@@ -12,39 +15,13 @@ import com.test.module.user.UserFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var fragmentContainerView: FragmentContainerView
+    private lateinit var navController: NavController
 
-     var int: Int=1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNavigationView=findViewById(R.id.bottom_navigation_view)
-        fragmentContainerView=findViewById(R.id.container_view)
-
-        val fragmentMap:MutableMap<Int, Fragment> =HashMap()
-        val homeFragment= HomeFragment()
-        val squareFragment=SquareFragment()
-        val discoveryFragment= DiscoveryFragment()
-        val userFragment=UserFragment()
-        fragmentMap[R.id.home]=homeFragment
-        fragmentMap[R.id.discovery]=discoveryFragment
-        fragmentMap[R.id.square]=squareFragment
-        fragmentMap[R.id.user]=userFragment
-
-        bottomNavigationView.setOnItemSelectedListener{item->
-            when(item.itemId){
-                R.id.home->supportFragmentManager.beginTransaction().replace(
-                    R.id.container_view,fragmentMap[R.id.home]!!).commit()
-                R.id.square->supportFragmentManager.beginTransaction().replace(
-                    R.id.container_view,fragmentMap[R.id.square]!!).commit()
-                R.id.discovery->supportFragmentManager.beginTransaction().replace(
-                    R.id.container_view,fragmentMap[R.id.discovery]!!).commit()
-                R.id.user->supportFragmentManager.beginTransaction().replace(
-                    R.id.container_view,fragmentMap[R.id.user]!!).commit()
-            }
-            true
-        }
-        supportFragmentManager.beginTransaction().replace(R.id.container_view,fragmentMap[R.id.home]!!).commit()
-        supportFragmentManager.beginTransaction().replace(R.id.container_view,fragmentMap[R.id.discovery]!!).commit()
+        navController=Navigation.findNavController(this,R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigationView,navController)
     }
 }
