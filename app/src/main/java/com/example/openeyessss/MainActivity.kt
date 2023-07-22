01aaf9a659266ba3603554eb30a3112e31e8b0be
Piskr0613 <2804239455@qq.com>
2023-07-22 09:59:29 +0800

    :lipstick: 统一了状态栏和tab栏的颜色

--- app/src/main/java/com/example/openeyessss/MainActivity.kt
+++ app/src/main/java/com/example/openeyessss/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.openEye)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
