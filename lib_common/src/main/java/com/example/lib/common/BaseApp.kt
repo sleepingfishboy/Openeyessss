22761b785707e8e74dee0b518286d6b8f5b67297
Piskr0613 <2804239455@qq.com>
2023-07-22 14:58:48 +0800

    :art: 多模块2.0

--- lib_common/src/main/java/com/example/lib/common/BaseApp.kt
+++ lib_common/src/main/java/com/example/lib/common/BaseApp.kt
@@ -1,9 +1,19 @@
 package com.example.lib.common
 
+import android.app.Application
+
 /**
  *作者：sleepingfishboy
  *时间：2023/7/22
 
  */
-class BaseApp {
+open class BaseApp: Application() {
+    companion object {
+        lateinit var mContext: BaseApp
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mContext=this
+    }
 }
