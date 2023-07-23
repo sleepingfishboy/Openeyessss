29be6cf2bef38cc70a55589b6e8eee53a45899f4
sleepingfishboy <2458699092@qq.com>
2023-07-23 18:32:59 +0800

    :sparkles: 完成视频播放界面和发现界面的联动视频

--- module_player/src/main/java/com/test/module/player/PlayerMainActivity.kt
+++ module_player/src/main/java/com/test/module/player/PlayerMainActivity.kt
@@ -3,29 +3,42 @@ package com.test.module.player
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
+import com.test.module.player.ApiRequest.apiService
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import xyz.doikki.videocontroller.StandardVideoController
 import xyz.doikki.videoplayer.player.VideoView
 
 
 @Route(path = "/player/activity", group = "player")
 class PlayerMainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
     private lateinit var videoView: VideoView
+    private var disposable: Disposable? = null
+    private lateinit var adapter: RelevantAdapter
 
     @Autowired
-    lateinit var url:String
+    lateinit var url: String
 
     @Autowired
-    lateinit var title:String
+    lateinit var title: String
+
     @Autowired
-    lateinit var description:String
+    lateinit var description: String
+
     @Autowired
-    lateinit var id:String
+    lateinit var id: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +52,21 @@ class PlayerMainActivity : AppCompatActivity() {
         ARouter.getInstance().inject(this)
         val des: TextView = findViewById(R.id.tv_cv_intro)
         des.text = description
+        if (id != null) {
+            recyclerView = findViewById(R.id.rv_relevant)
+            recyclerView.layoutManager = LinearLayoutManager(this)
 
+            adapter = RelevantAdapter()
+            recyclerView.adapter = adapter
+            disposable = ApiManager.getRelatedVideos(id)
+                ?.subscribeOn(Schedulers.io())
+                ?.observeOn(AndroidSchedulers.mainThread())
+                ?.subscribe({ relevantVideos ->
+                    adapter.setRelevantData(relevantVideos.itemList)
+                }, { error ->
+                    Log.d("ggg","(:)-->> rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+                })
+        }
 
         videoView = findViewById(R.id.player)
         videoView.setUrl(url) //设置视频地址
